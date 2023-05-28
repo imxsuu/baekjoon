223d8c907eddb04b32fbcf9ce2e4ae300015d358
imxsuu <imxsuu@gmaill.com>
2023-05-28 18:54:20 +0900

    Solve 12865

--- 12865.java
+++ 12865.java
@@ -1,35 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-	static int n,k;
-	static int dp[][],w[],v[]; // dp배열과 무게, 가치배열
 	public static void main(String[] args)   {
 		Scanner sc = new Scanner(System.in);
 		
-		n = sc.nextInt();
-		k = sc.nextInt();
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+        
+        // 1부터 n까지 저장하기 위해 배열 0번째 index를 버리고 n+1까지 생성
+		int[][] dp = new int[n+1][k+1];
+		int[] w =new int[n+1];
+		int[] v = new int[n+1];
 		
-		dp = new int[n+1][k+1];
-		
-		w =new int[n+1];
-		v = new int[n+1];
-		
-		for(int i=1;i<=n;i++) {			
+        // 물품의 수 조건: N(1 ≤ N ≤ 100)
+		for(int i = 1; i <= n; i++) {			
 			w[i] = sc.nextInt();
-			v[i]= sc.nextInt();
-		
+			v[i] = sc.nextInt();
 		}
 		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=k;j++) {
-				dp[i][j] = dp[i-1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
-				if(j - w[i]>=0) { // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
-					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]); // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치 중 큰 값을 취한다.
+        sc.close();
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= k; j++) {
+				dp[i][j] = dp[i-1][j]; // 기본적으로 이전 물품의 가치 저장
+				if(j - w[i] >= 0) { // 허용 가능한 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]); // 이전 물품의 가치 vs (남은 무게의 가치 + 자신의 가치) 중 큰 값을 선택
 				}
 			}
 		}
-		
-		System.out.println(dp[n][k]);
+		System.out.println(dp[n][k]);	
 	}
-	
 }
+
