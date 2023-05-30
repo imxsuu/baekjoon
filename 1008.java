ad5dd35ffd0ea7418791fcbb7e499d85a5d38d5e
imxsuu <imxsuu@gmaill.com>
2023-05-31 01:48:22 +0900

    Solve 1008, 10998

--- 1008.java
+++ 1008.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
         sc.close();
-
-        for(int i = 1; i <= n; i++) {
-            System.out.println(i);
-        }
+        
+        System.out.println(a / b);
     }
 }
